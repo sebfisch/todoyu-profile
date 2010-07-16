860b7cd0c5c86950ae52430baa70308338c73ca0
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-07-16 15:14:36 +0000

    * uncommitted formal cleanups

--- assets/js/Ext.js
+++ assets/js/Ext.js
@@ -1,9 +1,35 @@
+/****************************************************************************
+* todoyu is published under the BSD License:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* Copyright (c) 2010, snowflake productions gmbh
+* All rights reserved.
+*
+* This script is part of the todoyu project.
+* The todoyu project is free software; you can redistribute it and/or modify
+* it under the terms of the BSD License.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
+* for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script.
+*****************************************************************************/
+
 Todoyu.Ext.profile = {
 
 	PanelWidget: {},
 	
 	Headlet: {},
 
+
+
+	/**
+	 * Load given profile module
+	 *
+	 * @param	{String}	module
+	 */
 	loadModule: function(module) {
 		var url		= Todoyu.getUrl('profile', 'ext');
 		var options	= {
@@ -17,10 +43,23 @@ Todoyu.Ext.profile = {
 		Todoyu.Ui.updateContent(url, options);
 	},
 
+
+	/**
+	 *
+	 * @param	 {String}			module
+	 * @param	{Ajax.Response}		response
+	 */
 	onModuleLoaded: function(module, response) {
 
 	},
 
+
+
+	/**
+	 * Set body to given HTML content
+	 *
+	 * @param	{String}	content
+	 */
 	setContent: function(content) {
 		Todoyu.Ui.setContentBody(content);
 	}
--- assets/js/HeadletProfile.js
+++ assets/js/HeadletProfile.js
@@ -1,3 +1,22 @@
+/****************************************************************************
+* todoyu is published under the BSD License:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* Copyright (c) 2010, snowflake productions gmbh
+* All rights reserved.
+*
+* This script is part of the todoyu project.
+* The todoyu project is free software; you can redistribute it and/or modify
+* it under the terms of the BSD License.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
+* for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script.
+*****************************************************************************/
+
 Todoyu.Ext.profile.Headlet.Profile = {
 
 	/**
@@ -7,14 +26,24 @@ Todoyu.Ext.profile.Headlet.Profile = {
 	 */
 	ext: Todoyu.Ext.profile,
 
+
+
+	/**
+	 * Initialize
+	 */
 	init: function() {
 		
 	},
 
 
 
+	/**
+	 * Handler for button clicks - redirect to profile
+	 *
+	 * @param	{Object}	event
+	 */
 	onButtonClick: function(event) {
 		Todoyu.goTo('profile', 'ext');
 	}
-	
+
 };
